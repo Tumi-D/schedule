2d9be1e25d0371d61d64f8eeba8489f2a25e9a35
unknown <chris.debrah@yahoo.com>
2020-02-14 17:04:39 -0800

    Latest updtaes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -37,47 +37,65 @@
                 <div class="login100-form-title"
                     style="background-image: url({{asset('pages/images/login-banner.jpg')}});">
                     <span class="login100-form-title-1">
-                        Login In
+                        Login
                     </span>
                 </div>
 
-                <form class="login100-form validate-form">
+                <form method="POST" action="{{ route('login') }}" class="login100-form validate-form">
+                    @csrf
                     <div class="wrap-input100 validate-input m-b-26" data-validate="Username is required">
-                        <span class="label-input100">Username</span>
-                        <input class="input100" type="text" name="username" placeholder="Enter username">
+                        <span class="label-input100">Email </span>
+                        <input class="input100" type="text" name="email" placeholder="Enter username" autocomplete>
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
                         <span class="focus-input100"></span>
                     </div>
 
                     <div class="wrap-input100 validate-input m-b-18" data-validate="Password is required">
                         <span class="label-input100">Password</span>
-                        <input class="input100" type="password" name="pass" placeholder="Enter password">
+                        <input class="input100" type="password" name="password" placeholder="Enter password">
+                        @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
                         <span class="focus-input100"></span>
                     </div>
 
                     <div class="flex-sb-m w-full p-b-30">
                         <div class="contact100-form-checkbox">
-                            <input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
+                            <input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me"
+                                {{ old('remember') ? 'checked' : '' }} autocomplete>
                             <label class="label-checkbox100" for="ckb1">
                                 Remember me
                             </label>
                         </div>
-
+                        @if (Route::has('password.request'))
                         <div>
-                            <a href="#" class="txt1">
+                            <a href="{{ route('password.request') }}" class="txt1">
                                 Forgot Password?
                             </a>
                         </div>
+                        @endif
                     </div>
 
                     <div class="container-login100-form-btn">
                         <button class="login100-form-btn">
                             Login
                         </button>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <a href="{{url('/')}}" style="float:left">
+                            Back to home
+                        </a>
                     </div>
                 </form>
             </div>
         </div>
     </div>
+
 
     <!--===============================================================================================-->
     <script src="{{asset('pages/vendor/jquery/jquery-3.2.1.min.js')}}"></script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -62,8 +62,8 @@
         <div class="container d-flex h-100 align-items-center">
             <div class="mx-auto text-center">
                 <h1 class="mx-auto my-0 text-uppercase">Scheduler</h1>
-                <h2 class="text-white-50 mx-auto mt-2 mb-5">A free, responsive, one page Bootstrap theme created by
-                    Start Bootstrap.</h2>
+                <h2 class="text-white-50 mx-auto mt-2 mb-5">An online time management app for the EWC Protocol
+                    Department.</h2>
                 <a href="{{asset('pdf/EWC_Protocol_Friday_Schedule.pdf') }}" class="btn btn-primary js-scroll-trigger"
                     download="EWC_Protocol_Friday_Schedule.pdf">Download
                     Latest Schedule</a>
